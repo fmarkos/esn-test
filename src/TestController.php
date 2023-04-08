f58c0a5fa2bc3956a271c8d282d346e6617171f5
Markos Fragkoulopoulos <fmarkos@gmail.com>
2023-04-08 18:23:39 +0300

    Task2

--- src/TestController.php
+++ src/TestController.php
@@ -8,17 +8,45 @@ use EsnTest\Controller\EsnFetcher;
  * Class for the exercise.
  */
 class TestController extends EsnFetcher {
+  private $articles;
+  public function __construct()
+  {
+    $this->articles = json_decode(file_get_contents('src/Assets/news.json'));
+  }
+  /**
+   * Returns the needed data for solving task 2.
+   *
+   * You might need extra functions for this test.
+   *
+   * @return
+   *   The complete article data set.
+   */
+  public function getNews() {
+    return $this->articles;
+  }
+
+  /**
+   * Returns the needed data for solving task 2.
+   *
+   * You might need extra functions for this test.
+   *
+   * @return
+   *   The number of articles.
+   */
+  public function countNews() {
+    return count($num_articles);
+  }
 
   /**
    * Returns the needed data for solving task 4.
    *
    * You might need extra functions for this test.
    *
-   * @return 
+   * @return
    *   The data you decide to return.
    */
   public function getDataT4() {
-    
+
     return ;
   }
 
@@ -27,11 +55,11 @@ class TestController extends EsnFetcher {
    *
    * You might need extra functions for this test.
    *
-   * @return 
+   * @return
    *   The data you decide to return.
    */
   public function getDataT5() {
-      
+
     return ;
   }
 
